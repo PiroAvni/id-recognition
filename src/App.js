200dedb365070f171ca354dd293f163118129c56
PiroAvni <avnipiro.dev@gmail.com>
2022-07-19 00:25:59 +0100

    update

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://obscure-beach-29482.herokuapp.com:3000/image', {
+          fetch('https://obscure-beach-29482.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
